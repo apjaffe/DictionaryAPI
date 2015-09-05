7040dfa14d33fc7602a5a8b511cda25818799d09
A J <fortinbox123@googlemail.com>
2015-09-05 02:10:58 -0400

    Get difficulty endpoint

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +13,21 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/get_difficulty', function(req, resp) {
+    url = "http://dictionary.reference.com/browse/" + req.query.word;
+
+    request(url, function(error, inner_resp, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            var difficulty = $('#difficulty-box').attr("data-difficulty");
+            resp.json({"difficulty": difficulty});
+        }
+        else {
+            resp.json({"error": error});
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
