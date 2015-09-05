89cdfcaf2eb17079d85626e7da591d2095aa3ddc
tomtseng <thomasts@andrew.cmu.edu>
2015-09-05 18:27:56 -0400

    Add get_synonyms (plural)

--- index.js
+++ index.js
@@ -186,6 +186,20 @@ app.get('/get_synonym', function(req, resp) {
     });
 });
 
+app.post('/get_synonyms', function(req, resp) {
+    var words = JSON.parse(req.body.words);
+    var dict = {};
+    var count = 0;
+    for (var i = 0; i < words.length; i++) {
+        get_synonym(words[i], (function(index) { return function(synonym) {
+            dict[words[index]] = synonym;
+            count++;
+            if (count === words.length) {
+                resp.json(dict);
+            }
+        }})(i));
+    }
+});
 
 //Use post requests to handle large numbers of words
 app.post('/get_definitions', function(req, resp) {
